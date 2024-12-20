481e1b200c216079a4339dc304e062ab0138ed3a
Naci Gokmen <nacigokmen99@gmail.com>
2024-12-21 00:52:27 +0100

    basic calculator

---
+++ calculator.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int number1, number2, result;
+    char tegn;
+
+    cout << "Number 1 :";
+    cin >> number1;
+
+    cout << "Number 2 :";
+    cin >> number2;
+
+    cout << "Operator (+,-,/,*,%) =";
+    cin >> tegn;
+
+    switch (tegn)
+    {
+    case '+':
+        cout << "Result= " << number1 + number2 << endl;
+        break;
+
+    case '-':
+        cout << "Result= " << number1 - number2 << endl;
+        ;
+        break;
+
+    case '/':
+        if (number2 != 0)
+        {
+            cout << "Result= " << number1 / number2 << endl;
+            ;
+        }
+        else
+            cout << "Error: Division by zero!" << endl;
+        break;
+
+    case '*':
+        cout << "Result= " << number1 * number2 << endl;
+        ;
+        break;
+
+    case '%':
+        if (number2 != 0)
+        {
+            cout << "Result= " << number1 % number2 << endl;
+            ;
+        }
+        else
+            cout << "Error: Division by zero!" << endl;
+        break;
+
+    default:
+        cout << "Error: Invalid operator!" << endl;
+        break;
+    }
+
+    return 0;
+}
